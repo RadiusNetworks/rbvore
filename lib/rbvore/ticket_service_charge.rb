c202138cda970032c3c346373b614e1deac98e91
Scott Yoder <yodersb@gmail.com>
2019-12-03 11:57:28 -0500

    allow adding service charges to ticket

--- lib/rbvore/ticket_service_charge.rb
+++ lib/rbvore/ticket_service_charge.rb
@@ -7,5 +7,17 @@ module Rbvore
     def initialize(hash)
       set_attributes(hash)
     end
+
+    def self.create(location_id:, ticket_id:, name:, amount:, api_key: nil)
+      response = api.request(
+        :post,
+        API.endpoint(Location, location_id, Ticket, ticket_id, "service_charges"),
+        body: [{ service_charge: name, amount: amount }],
+        api_key: api_key,
+      )
+      raise response.error unless response.success?
+
+      TicketServiceCharge.new(response.json_body)
+    end
   end
 end
